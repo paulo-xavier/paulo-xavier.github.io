a2f0d318421acf05b68a92734264546d97757922
paulo-xavier <px568661@gmail.com>
2024-01-12 16:45:46 -0300

    :lipstick: feat: New projects in the project page.
    Advice generator and Dashboard for register of people and companies were added on the projects page.

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,49 +4,68 @@ const dialogWeather = document.getElementById('dialog-weather');
 const dialogBahSocorro = document.getElementById('dialog-bahsocorro'); 
 const dialogRagnarok = document.getElementById('dialog-ragnarok'); 
 const dialogNotificationsPage = document.getElementById('dialog-notifications-page'); 
+const dialogDashboard = document.getElementById('dialog-dashboard'); 
+const dialogAdviceGenerator = document.getElementById('dialog-advice-generator'); 
+
 
 const dialogContainerButton = document.querySelectorAll('#dialog-container-button'); 
+
 
 let dialogIsOpen = false; 
 
 imageContainer.forEach((image, index) => image.addEventListener ('click' , () =>  {
         if (dialogIsOpen == false) {
 
-            if (index == 0 ) {
+            if (index == 0) {
+                dialogDashboard.open = true; 
+                dialogIsOpen = true
+            
+            }  else if (index == 1 ) {
                 dialogWeather.open = true; 
                 dialogIsOpen = true; 
             
-            } else if (index == 1) {
+            } else if (index == 2) {
+                dialogAdviceGenerator.open = true; 
+                dialogIsOpen = true; 
+
+            } else if (index == 3) {
                 dialogBahSocorro.open = true; 
                 dialogIsOpen = true; 
-            
-            } else if (index == 2) {
+                
+            } else if (index == 4) {
                 dialogRagnarok.open = true;
                 dialogIsOpen = true; 
-            
-            } else if (index == 3) {
+                
+            } else if (index == 5) {
                 dialogNotificationsPage.open = true; 
-                dialogIsOpen = true; 
-            } 
-
+                dialogIsOpen = true;
+            }
         } 
-
 })); 
 
 dialogContainerButton.forEach((dialog, index) => dialog.addEventListener('click', () => {
-        if (index == 0){
+        
+        if (index == 0) {
+            dialogDashboard.open = false; 
+            dialogIsOpen = false; 
+        
+        } else if (index == 1){
             dialogWeather.open = false; 
             dialogIsOpen = false; 
 
-        } else if (index == 1) {
+        } else if (index == 2) {
+            dialogAdviceGenerator.open = false; 
+            dialogIsOpen = false; 
+
+        } else if (index == 3) {
             dialogBahSocorro.open = false; 
             dialogIsOpen = false; 
         
-        } else if (index == 2) {
+        } else if (index == 4) {
             dialogRagnarok.open = false; 
             dialogIsOpen = false; 
         
-        } else if (index == 3) {
+        } else if (index == 5) {
             dialogNotificationsPage.open = false; 
             dialogIsOpen = false; 
         }
